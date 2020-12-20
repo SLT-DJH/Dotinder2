b2de88529b89dd6bd83f789c3f16fd67110851e6
SLT-DJH <jydwww@naver.com>
2020-12-20 23:27:39 +0900

    user Authentication, Gender firestore

--- app/src/main/java/com/jinhyun/dotinder/MainActivity.kt
+++ app/src/main/java/com/jinhyun/dotinder/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.jinhyun.dotinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.lorentzos.flingswipe.SwipeFlingAdapterView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     var al = ArrayList<String>()
     var arrayAdapter: ArrayAdapter<String>? = null
     var i: Int = 0
+
+    val mAuth = FirebaseAuth.getInstance()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,11 +46,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onLeftCardExit(p0: Any?) {
-                Toast.makeText(applicationContext, "LEFT!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, R.string.dislike, Toast.LENGTH_SHORT).show()
             }
 
             override fun onRightCardExit(p0: Any?) {
-                Toast.makeText(applicationContext, "RIGHT!",Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, R.string.like,Toast.LENGTH_SHORT).show()
             }
 
             override fun onAdapterAboutToEmpty(p0: Int) {
@@ -69,6 +73,21 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Clicked!",Toast.LENGTH_SHORT).show()
             }
         })
+
+        btn_left.setOnClickListener {
+            flingContainer.topCardListener.selectLeft()
+        }
+
+        btn_right.setOnClickListener {
+            flingContainer.topCardListener.selectRight()
+        }
+
+        btn_signout.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(this, LoginRegistrationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
--- app/src/main/java/com/jinhyun/dotinder/RegistrationActivity.kt
+++ app/src/main/java/com/jinhyun/dotinder/RegistrationActivity.kt
@@ -3,9 +3,11 @@ package com.jinhyun.dotinder
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
@@ -21,6 +23,7 @@ class RegistrationActivity : AppCompatActivity() {
             finish()
         }
     }
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,16 +41,43 @@ class RegistrationActivity : AppCompatActivity() {
         btn_registration.setOnClickListener {
             val email = et_email_regi.text.toString()
             val password = et_password_regi.text.toString()
+            val name = et_name.text.toString()
+            val selectGender = rg_gender.checkedRadioButtonId
+            val rbtn : RadioButton = findViewById(selectGender)
 
             if (email.isEmpty() || password.isEmpty()){
-                Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.request_email_pw, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (name.isEmpty()){
+                Toast.makeText(this, R.string.request_name, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (rbtn.text == null){
+                Toast.makeText(this, R.string.request_gender, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                if(!it.isSuccessful) return@addOnCompleteListener
-
-                Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                if(!it.isSuccessful){
+                    return@addOnCompleteListener
+                }else{
+                    val userId = it.result?.user?.uid.toString()
+                    val userinfo = hashMapOf(
+                        "name" to name,
+                        "email" to email,
+                        "password" to password,
+                        "uid" to userId
+                    )
+                    db.collection(rbtn.text.toString()).document(userId).set(userinfo).addOnSuccessListener {
+                        Toast.makeText(this, R.string.register_succeed, Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener {
+                        Toast.makeText(this, R.string.register_failed, Toast.LENGTH_SHORT).show()
+                    }
+                    Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
             }.addOnFailureListener{
                 Log.d(TAG, "Failed to create user: ${it.message}")
             }
